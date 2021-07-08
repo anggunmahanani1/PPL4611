0ede5442a915eba1653357a6fd682ac4dc7b2846
Sapto Susilo <80048581+kuclukbos@users.noreply.github.com>
2021-07-08 08:41:44 +0700

    Update dreadout.php

--- gameina-master/application/views/store/dreadout.php
+++ gameina-master/application/views/store/dreadout.php
@@ -230,164 +230,7 @@
                     <!-- begin:: Header Topbar -->
                     <div class="kt-header__topbar kt-grid__item">
 
-                        <!--begin: Search -->
-                        <!-- <div class="kt-header__topbar-item kt-header__topbar-item--search dropdown" id="kt_quick_search_toggle">
-                            <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px">
-                                <span class="kt-header__topbar-icon"><i class="flaticon2-search-1"></i></span>
-                            </div>
-                            <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-lg">
-                                <div class="kt-quick-search kt-quick-search--inline" id="kt_quick_search_inline">
-                                    <form method="get" class="kt-quick-search__form">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend"><span class="input-group-text"><i class="flaticon2-search-1"></i></span></div>
-                                            <input type="text" class="form-control kt-quick-search__input" placeholder="Search...">
-                                            <div class="input-group-append"><span class="input-group-text"><i class="la la-close kt-quick-search__close"></i></span></div>
-                                        </div>
-                                    </form>
-                                    <div class="kt-quick-search__wrapper kt-scroll" data-scroll="true" data-height="300" data-mobile-height="200">
-                                    </div>
-                                </div>
-                            </div>
-                        </div> -->
-
-                        <!--end: Search -->
-
-                        <!--begin: Cart -->
-                        <!-- <div class="kt-header__topbar-item dropdown">
-                            <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px" aria-expanded="true">
-                                <span class="kt-header__topbar-icon"><i class="flaticon2-shopping-cart-1"></i></span>
-                            </div>
-                            <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
-                                <form> -->
-
-                                    <!-- begin:: Mycart -->
-                                    <!-- <div class="kt-mycart">
-                                        <div class="kt-mycart__head kt-head" style="background-image: url(assets/media/misc/bg-1.jpg);">
-                                            <div class="kt-mycart__info">
-                                                <span class="kt-mycart__icon"><i class="flaticon2-shopping-cart-1 kt-font-success"></i></span>
-                                                <h3 class="kt-mycart__title">My Cart</h3>
-                                            </div>
-                                            <div class="kt-mycart__button">
-                                                <button type="button" class="btn btn-success btn-sm" style=" ">2
-                                                    Items</button>
-                                            </div>
-                                        </div>
-                                        <div class="kt-mycart__body kt-scroll" data-scroll="true" data-height="245" data-mobile-height="200">
-                                            <div class="kt-mycart__item">
-                                                <div class="kt-mycart__container">
-                                                    <div class="kt-mycart__info">
-                                                        <a href="#" class="kt-mycart__title">
-                                                            Samsung
-                                                        </a>
-                                                        <span class="kt-mycart__desc">
-                                                            Profile info, Timeline etc
-                                                        </span>
-                                                        <div class="kt-mycart__action">
-                                                            <span class="kt-mycart__price">$ 450</span>
-                                                            <span class="kt-mycart__text">for</span>
-                                                            <span class="kt-mycart__quantity">7</span>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                                                        </div>
-                                                    </div>
-                                                    <a href="#" class="kt-mycart__pic">
-                                                        <img src="<?= base_url('assets/') ?>assets/media/products/product9.jpg" title="">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                            <div class="kt-mycart__item">
-                                                <div class="kt-mycart__container">
-                                                    <div class="kt-mycart__info">
-                                                        <a href="#" class="kt-mycart__title">
-                                                            Panasonic
-                                                        </a>
-                                                        <span class="kt-mycart__desc">
-                                                            For PHoto & Others
-                                                        </span>
-                                                        <div class="kt-mycart__action">
-                                                            <span class="kt-mycart__price">$ 329</span>
-                                                            <span class="kt-mycart__text">for</span>
-                                                            <span class="kt-mycart__quantity">1</span>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                                                        </div>
-                                                    </div>
-                                                    <a href="#" class="kt-mycart__pic">
-                                                        <img src="<?= base_url('assets/') ?>assets/media/products/product13.jpg" title="">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                            <div class="kt-mycart__item">
-                                                <div class="kt-mycart__container">
-                                                    <div class="kt-mycart__info">
-                                                        <a href="#" class="kt-mycart__title">
-                                                            Fujifilm
-                                                        </a>
-                                                        <span class="kt-mycart__desc">
-                                                            Profile info, Timeline etc
-                                                        </span>
-                                                        <div class="kt-mycart__action">
-                                                            <span class="kt-mycart__price">$ 520</span>
-                                                            <span class="kt-mycart__text">for</span>
-                                                            <span class="kt-mycart__quantity">6</span>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                                                        </div>
-                                                    </div>
-                                                    <a href="#" class="kt-mycart__pic">
-                                                        <img src="<?= base_url('assets/') ?>assets/media/products/product16.jpg" title="">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                            <div class="kt-mycart__item">
-                                                <div class="kt-mycart__container">
-                                                    <div class="kt-mycart__info">
-                                                        <a href="#" class="kt-mycart__title">
-                                                            Candy Machine
-                                                        </a>
-                                                        <span class="kt-mycart__desc">
-                                                            For PHoto & Others
-                                                        </span>
-                                                        <div class="kt-mycart__action">
-                                                            <span class="kt-mycart__price">$ 784</span>
-                                                            <span class="kt-mycart__text">for</span>
-                                                            <span class="kt-mycart__quantity">4</span>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&minus;</a>
-                                                            <a href="#" class="btn btn-label-success btn-icon">&plus;</a>
-                                                        </div>
-                                                    </div>
-                                                    <a href="#" class="kt-mycart__pic">
-                                                        <img src="<?= base_url('assets/') ?>assets/media/products/product15.jpg" title="" alt="">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="kt-mycart__footer">
-                                            <div class="kt-mycart__section">
-                                                <div class="kt-mycart__subtitel">
-                                                    <span>Sub Total</span>
-                                                    <span>Taxes</span>
-                                                    <span>Total</span>
-                                                </div>
-                                                <div class="kt-mycart__prices">
-                                                    <span>$ 840.00</span>
-                                                    <span>$ 72.00</span>
-                                                    <span class="kt-font-brand">$ 912.00</span>
-                                                </div>
-                                            </div>
-                                            <div class="kt-mycart__button kt-align-right">
-                                                <button type="button" class="btn btn-primary btn-sm">Place
-                                                    Order</button>
-                                            </div>
-                                        </div>
-                                    </div> -->
-
-                                    <!-- end:: Mycart -->
-                                <!-- </form>
-                            </div>
-                        </div> -->
-
-                        <!--end: Cart -->
+              
 
                         <!--begin: User bar -->
                         <div class="kt-header__topbar-item kt-header__topbar-item--user">
@@ -426,76 +269,18 @@
 
                                 <!--end: Head -->
 
-                                <!--begin: Navigation -->
-                                <div class="kt-notification">
-                                    <a href="#" class="kt-notification__item">
-                                        <div class="kt-notification__item-icon">
-                                            <i class="flaticon2-calendar-3 kt-font-success"></i>
-                                        </div>
-                                        <div class="kt-notification__item-details">
-                                            <div class="kt-notification__item-title kt-font-bold">
-                                                Profil saya
-                                            </div>
-                                            <div class="kt-notification__item-time">
-                                                Pengaturan Akun dan lainnya
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="kt-notification__item">
-                                        <div class="kt-notification__item-icon">
-                                            <i class="flaticon2-mail kt-font-warning"></i>
-                                        </div>
-                                        <div class="kt-notification__item-details">
-                                            <div class="kt-notification__item-title kt-font-bold">
-                                                Pesan saya
-                                            </div>
-                                            <div class="kt-notification__item-time">
-                                                Kotak masuk dan tugas
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="kt-notification__item">
-                                        <div class="kt-notification__item-icon">
-                                            <i class="flaticon2-rocket-1 kt-font-danger"></i>
-                                        </div>
-                                        <div class="kt-notification__item-details">
-                                            <div class="kt-notification__item-title kt-font-bold">
-                                                Aktivitas saya
-                                            </div>
-                                            <div class="kt-notification__item-time">
-                                                Log dan notifikasi
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="kt-notification__item">
-                                        <div class="kt-notification__item-icon">
-                                            <i class="flaticon2-hourglass kt-font-brand"></i>
-                                        </div>
-                                        <div class="kt-notification__item-details">
-                                            <div class="kt-notification__item-title kt-font-bold">
-                                                Keranjang saya
-                                            </div>
-                                            <div class="kt-notification__item-time">
-                                                As you wish!
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <div class="kt-notification__custom">
-                                        <a href="<?= base_url('welcome/logout') ?>" target=" _blank" class="btn btn-label-brand btn-sm btn-bold">Log Out</a>
-                                    </div>
-                                </div>
-
-                                <!--end: Navigation -->
+                            
                             </div>
                         </div>
 
                         <!--end: User bar -->
 
                         <!--begin: Quick panel toggler -->
-                        <div class="kt-header__topbar-item" data-toggle="kt-tooltip" title="Quick panel" data-placement="top">
+                        <div class="kt-header__topbar-item" data-toggle="kt-tooltip" title="Log Out" data-placement="top">
                             <div class="kt-header__topbar-wrapper">
-                                <span class="kt-header__topbar-icon" id="kt_quick_panel_toggler_btn"><i class="flaticon2-cube-1"></i></span>
+                                <span class="kt-header__topbar-icon" id="kt_quick_panel_toggler_btn"><a href="<?= base_url('welcome/logout') ?>" target=" _blank" class="btn btn-label-brand btn-sm btn-bold">Log Out</a></span>
                             </div>
+
                         </div>
 
                         <!--end: Quick panel toggler -->

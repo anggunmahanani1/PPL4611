d1bd57fcf25339ee1b06fabe73ad94d22f538264
Sapto Susilo <80048581+kuclukbos@users.noreply.github.com>
2021-07-08 07:10:49 +0700

    Update footer.php

--- gameina-master/application/views/templates/footer.php
+++ gameina-master/application/views/templates/footer.php
@@ -1,5 +1,18 @@
 <!-- Footer -->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <script src="https://kit.fontawesome.com/57635d64f6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>css/styling.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <script src="<?= base_url('assets/') ?>sweetalert/package/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>sweetalert/package/dist/sweetalert2.min.css">
+    <link href=" https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <footer class="page-footer font-small pt-4 mt-5">
     <div class="container">
         <!-- Footer Links -->
